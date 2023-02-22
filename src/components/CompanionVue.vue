a55d91f6b04e69a31f887d02793fffc38e133652
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-02-22 17:17:23 -0500

    fix: companion `the` computed

--- src/components/CompanionVue.vue
+++ src/components/CompanionVue.vue
@@ -1,12 +1,16 @@
 <template>
 	<br>
 	<br>
-	It is the {{ computed_text }} companion to the <IHref :repo='repo' />
+	It is the {{ computed_text }} companion to  {{ the }}<IHref :repo='repo' />
 </template>
 
 <script setup lang="ts">
 import IHref from '@/components/IHref.vue';
 import type { TGithubRepos } from '@/types';
+
+const the = computed((): string => {
+	return props.repo == 'staticpi_backend' ? `` : `the `;
+});
 
 const computed_text = computed((): string => {
 	return props.repo.endsWith('_vue') ? 'backend' : 'frontend';
